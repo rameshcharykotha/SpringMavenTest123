6aa8c2203402674a62d1bf5a466b4da1aec68672
rameshcharykotha <rameshcharykotha@gmail.com>
2021-11-20 20:37:42 -0600

    test

---
+++ src/main/java/com/test/TestClass.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class TestClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        print();
+    }
+
+    public static void print(){
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/TestJava.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class TestJava {
+
+    @Test
+    public void testMethod(){
+        System.out.println("Hello World from Test");
+    }
+}
